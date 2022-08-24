15b123d728056a0188b3a7b700abf24012de2e78
hyosung <dlgytjdsla@naver.com>
2022-08-25 01:08:27 +0900

    댓글 수정

--- src/components/BoardDetail.jsx
+++ src/components/BoardDetail.jsx
@@ -1,14 +1,50 @@
 import axios from "axios";
-import { useLayoutEffect, useState } from "react";
+import { useContext, useEffect, useLayoutEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import UserInfoContext from "../context/UserInfoContext";
 
 const BoardDetail = () => {
+  const jwt = window.localStorage.getItem("jwt");
   const location = useLocation();
+  const { userInfo } = useContext(UserInfoContext);
+  const { status, username, usergender } = userInfo;
   const { board_id } = location.state;
 
   const [detail, setDetail] = useState({ status: false });
+  const [comment, setComment] = useState("");
+  const [addCommentStatus, setAddCommentStatus] = useState(false);
+
+  const addComment = () => {
+    if (comment.length === 0) alert("댓글을 작성해 주세요.");
+    console.log(username, comment, board_id);
+    axios({
+      url: "http://localhost:3000/board/history/comment",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      data: {
+        board_id,
+        username,
+        content: comment,
+      },
+    })
+      .then((res) => {
+        console.warn(res);
+        const { status, data } = res;
+        if (status === 201) {
+          setAddCommentStatus((status) => false);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   useLayoutEffect(() => {
+    console.log("console.log(username, comment, board_id);");
     axios({
       url: "http://localhost:3000/board/histroy/detail",
       method: "GET",
@@ -24,12 +60,13 @@ const BoardDetail = () => {
         const { status, data } = res;
         if (status === 200) {
           setDetail({ status: true, ...data });
+          setAddCommentStatus((status) => true);
         }
       })
       .catch((err) => {
         console.error(err);
       });
-  }, []);
+  }, [addCommentStatus]);
 
   return (
     <div>
@@ -44,8 +81,17 @@ const BoardDetail = () => {
           </p>
           <p>작성자: {detail.username}</p>
           <p>수정시간: {detail.updateAt}</p>
-          <p>
-            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 답글:{" "}
+          <div>
+            <div>
+              <div>
+                답글:{" "}
+                <input
+                  type={"text"}
+                  onChange={({ target }) => setComment(target.value)}
+                />
+                <button onClick={() => addComment()}>확인</button>
+              </div>
+            </div>
             {detail.__comments__.length > 0 &&
               detail.__comments__.map((data) => {
                 return (
@@ -58,8 +104,7 @@ const BoardDetail = () => {
                   </div>
                 );
               })}
-            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-          </p>
+          </div>
         </div>
       )}
     </div>
